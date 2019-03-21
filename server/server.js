cb509d11a15ca25a3e71fc08ac36cb0e56972f34
Zyh <zhangyinghaoweb@163.com>
2019-03-21 17:51:58 +0800

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const bodyPaeser = require('body-parser')
 const cookiePaeser = require('cookie-parser')
+const app = express()
+// work with express
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+io.on('connection', function(socket){
+    console.log('user login')
+    socket.on('sendmsg', function(data){
+        console.log(data)
+    })
+})
+
 const userRouter = require('./user')
 
-const app = express()
+
 // 可以解析cookie
 app.use(cookiePaeser())
 // 可以解析post请求传过来的json格式参数
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
-
+import { List, InputItem } from 'antd-mobile'
+import io from 'socket.io-client'
+const socket = io('ws://localhost:9093/socket.io', {
+  transports: ['websocket']
+})
+socket.on('connect',function(){
+    console.log('connected')
+})
 class Chat extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            text: ''
+        }
+    }
+    componentDidMount() {
+       
+    }
+    handelSubmit() {
+        socket.emit('sendmsg',{text:this.state.text})
+        this.setState({text:''})
+        console.log(this.state)
+    }
     render() {
         return (
-            <div>
+            <div className="stick-footer">
+            <List>
+                <InputItem
+                    placeholder="请输入"
+                    value={this.state.text}
+                    onChange={v=>{
+                        this.setState({text:v})
+                    }}
+                    extra={<span onClick={() =>this.handelSubmit()}>发送</span>}
+                ></InputItem>
+            </List>
                 chat{this.props.match.params.user}
             </div>
         )
